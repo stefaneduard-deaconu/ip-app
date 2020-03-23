75a89dc2bfd564a8444e69a2b581db8cd0deaf91
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-03-23 11:50:49 +0200

    tried base64 for image storing. For the sample it takes 40% more memory than the binary, but it's SQL compliant :))

--- app/src/main/java/com/example/ipapp/MainActivity.kt
+++ app/src/main/java/com/example/ipapp/MainActivity.kt
@@ -1,35 +1,29 @@
 package com.example.ipapp
 
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.webkit.MimeTypeMap
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-
-import androidx.core.content.FileProvider
 import androidx.core.content.FileProvider.getUriForFile
-
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-
 import com.example.ipapp.ui.home.HomeFragmentDirections
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 import java.io.File
 import java.io.IOException
-import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -92,8 +86,16 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            picture.setImageURI(Uri.parse(currentPhotoPath))
+//            picture.setImageURI(Uri.parse(currentPhotoPath))
 
+            val bitmap = BitmapFactory.decodeFile(currentPhotoPath)
+            val bytes = File(currentPhotoPath).readBytes()
+            val base64 = Base64.getEncoder().encodeToString(bytes)
+            val ext = MimeTypeMap.getFileExtensionFromUrl(currentPhotoPath)
+            val decoded = Base64.getDecoder().decode(base64)
+            val bitmap2 = BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
+//            TODO add this to the sqlite DB that you're going to implement :D
+            picture.setImageBitmap(bitmap2)
         }
     }
 
